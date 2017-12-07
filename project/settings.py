4f91302a09a8cdc684ac24a6b6d8b9f737df780d
Fedor Butikov <butikov@phystech.edu>
2017-12-08 01:08:58 +0300

    fix gdal?
    fix gdalagain
    
    another try
    
    added static files
    
    gdal...
    
    gdal2
    
    statics
    
    gdal..
    
    geos
    
    statics
    
    make it better
    
    favicon
    
    gdal again
    
    gdal again
    
    gdal again
    
    another try
    
    host allowed
    
    try
    
    try
    
    try to fix
    
    try
    
    hosts
    
    hosts
    
    hosts
    
    hosts
    
    hosts
    
    hosts
    
    hosts
    
    hosts
    
    migrate
    
    db fix
    
    db
    
    ewc
    
    aafw
    
    check
    
    true
    
    disable debug
    
    pointField
    
    db
    
    db
    
    db
    
    db
    
    try to fix dbs again
    
    try to fix dbs again
    
    try to fix dbs again
    
    db
    
    db
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    reqs
    
    static

--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+import dj_database_url
 import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,12 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '4i6pe8)mlr28ydh9o&8i2qu((s4i&m0ui)nc0(9+1+bq)0wu=d'
+#SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -49,6 +52,7 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'core.User'
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -88,13 +92,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'track',
-        'USER': 'fedor',
-        #      'PASSWORD': 'rodef',
-        #      'HOST': 'localhost',
+        'NAME': 'denro17kd0mqu8',
+        'USER': 'hgwfslhucwckzw',
+        'PASSWORD': '50e9f4367be1f965d0ebaf452954a6419cb08ac80d97853c471758c628e267f7',
+        'HOST': 'ec2-54-221-244-196.compute-1.amazonaws.com',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -124,11 +128,8 @@ INTERNAL_IPS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -136,16 +137,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/uploads/'
 
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-]
-
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 GOOGLE_MAP_API_KEY = "AIzaSyBKoBqoNECkRJCdgNvm3y0LclLCyoGfUdY"
 
 MAP_WIDGETS = {
@@ -157,3 +157,13 @@ MAP_WIDGETS = {
     ),
     "GOOGLE_MAP_API_KEY": GOOGLE_MAP_API_KEY,
 }
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+
+DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+print(DATABASES['default']['ENGINE'])
