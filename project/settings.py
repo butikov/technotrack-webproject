0d70c2858f6f774c86d0ffe4ac2bff694365e6a8
Fedor Butikov <butikov@phystech.edu>
2017-12-08 12:58:31 +0300

    aafw

--- project/settings.py
+++ project/settings.py
@@ -163,5 +163,6 @@ application = DjangoWhiteNoise(application)
 
 DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
