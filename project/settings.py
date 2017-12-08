40ccb662e34552639dbe44d9049db3d070863eb6
Fedor Butikov <butikov@phystech.edu>
2017-12-08 03:44:37 +0300

    gdal again

--- project/settings.py
+++ project/settings.py
@@ -14,8 +14,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-GDAL_LIBRARY_PATH = "/app/.heroku/vendor/lib/libgdal.so"
-GEOS_LIBRARY_PATH = "/app/.heroku/vendor/lib/libgeos_c.so"
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
