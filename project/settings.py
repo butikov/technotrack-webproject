d2e36bd6bb5da892a959abb58809d47378111f3f
Fedor Butikov <butikov@phystech.edu>
2017-12-08 13:12:27 +0300

    true

--- project/settings.py
+++ project/settings.py
@@ -162,6 +162,7 @@ application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
 
 DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['ATOMIC_REQUESTS'] = True
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
